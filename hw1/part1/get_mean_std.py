70f2361fa8717c60ed0e701def72b9525776b55f
IanLynnEE <kmes50311@gmail.com>
2022-10-07 06:40:21 +0800

    Fix mean and std

--- hw1/part1/get_mean_std.py
+++ hw1/part1/get_mean_std.py
@@ -4,8 +4,8 @@ import numpy as np
 
 from myDatasets import part1_dataset
 
-def gen_mean_std(dataset):
-    dataloader = DataLoader(train_set, batch_size=25, shuffle=False)
+def get_mean_std(dataset):
+    dataloader = DataLoader(train_set, batch_size=22500, shuffle=False)
     train = next(iter(dataloader))[0]
     mean = np.mean(train.numpy(), axis=(0, 2, 3))
     std = np.std(train.numpy(), axis=(0, 2, 3))
@@ -13,5 +13,5 @@ def gen_mean_std(dataset):
 
 if __name__=='__main__':
     train_set = part1_dataset(prefix='./data/p1_data/train_50')
-    mean, std = gen_mean_std(train_set)
+    mean, std = get_mean_std(train_set)
     print(mean, std)
