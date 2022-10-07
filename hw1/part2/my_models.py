f9056c3338043afd8d720d1e996ced036e22423f
IanLynnEE <kmes50311@gmail.com>
2022-10-07 19:44:59 +0800

    Fix VGG16-FCN8

--- hw1/part2/my_models.py
+++ hw1/part2/my_models.py
@@ -31,16 +31,10 @@ class VGG16_FCN32(nn.Module):
         return x
 
 
-# ref: https://blog.csdn.net/gbz3300255/article/details/105582572
 class VGG16_FCN8(nn.Module):
     def __init__(self, num_classes=7):
         super().__init__()
-        feats = list(vgg16(weights=VGG16_Weights.IMAGENET1K_FEATURES).features.children())
-        self.feat1 = nn.Sequential(*feats[0:4])
-        self.feat2 = nn.Sequential(*feats[5:9])
-        self.feat3 = nn.Sequential(*feats[10:16])
-        self.feat4 = nn.Sequential(*feats[17:23])
-        self.feat5 = nn.Sequential(*feats[24:30])
+        self.feats = vgg16(weights=VGG16_Weights.IMAGENET1K_FEATURES).features
 
         self.classifier = nn.Sequential(
             nn.Conv2d(in_channels=512, out_channels=4096, kernel_size=2),
@@ -51,22 +45,22 @@ class VGG16_FCN8(nn.Module):
             nn.Dropout2d(),
             nn.Conv2d(in_channels=4096, out_channels=num_classes, kernel_size=1)
         )
-        self.score_feat3 = nn.Conv2d(256, num_classes, 1)
-        self.score_feat4 = nn.Conv2d(512, num_classes, 1)
-        self.upscore2 = nn.ConvTranspose2d(num_classes, num_classes, 4, stride=2, bias=False)
-        self.upscore4 = nn.ConvTranspose2d(num_classes, num_classes, 16, stride=8, bias=False)
-        self.upscore8 = nn.ConvTranspose2d(num_classes, num_classes, 4, stride=2, bias=False)
+        self.upsample4 = nn.ConvTranspose2d(num_classes, 256, 8, stride=4, bias=False)
+        self.upsample2 = nn.ConvTranspose2d(512, 256, 2, stride=2, bias=False)
+        self.upsample8 = nn.ConvTranspose2d(256, num_classes, 8, stride=8, bias=False)
 
     def forward(self, x):
-        x1 = self.feat1(x)      # 1/2
-        x2 = self.feat2(x1)     # 1/4
-        x3 = self.feat3(x2)     # 1/8
-        x4 = self.feat4(x3)     # 1/16
-        x5 = self.feat5(x4)     # 1/32
-        score = self.classifier(x5)
-        score_x3 = self.score_feat3(x3)
-        score_x4 = self.score_feat3(x4)
-        upscore2 = self.upscore2(score)
-        upscore4 = self.upscore4(upscore2 + score_x4)
-        upscore8 = self.upscore8(upscore4 + score_x3)
-        return upscore8
+        for idx, feature in enumerate(self.feats):
+            x = feature(x)
+            if idx == 16:
+                pool3 = x
+            elif idx == 23:
+                pool4 = x
+        score = self.classifier(x)
+        # print('score', score.shape)
+        # print('pool3', pool3.shape)
+        # print('pool4', pool4.shape)
+        upsample_score = self.upsample4(score)
+        upsample_pool4 = self.upsample2(pool4)
+        upsample8 = self.upsample8(pool3 + upsample_pool4 + upsample_score)
+        return upsample8
