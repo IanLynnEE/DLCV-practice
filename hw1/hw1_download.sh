32b39bf8c0c0e5262343589133eaa68cecaaa5b4
IanLynnEE <kmes50311@gmail.com>
2022-10-10 17:14:43 +0800

    Finish inference script

--- hw1/hw1_download.sh
+++ hw1/hw1_download.sh
@@ -2,5 +2,5 @@
 
 mkdir saved_models/
 
-wget -o saved_models/ResNet50.pt
-wget -o saved_models/VGG16_FCN8s.pt
+wget -O "saved_models/ResNet50.pt" "https://www.dropbox.com/s/qruss095itgwfbi/ResNet50.pt?dl=1"
+wget -O "saved_models/VGG16_FCN8s.pt" "https://www.dropbox.com/s/azr64wjv65005ai/VGG16_FCN8s.pt?dl=1"
