6e3b7d8863b35bd76da38b96331d45c00ab03a26
IanLynnEE <kmes50311@gmail.com>
2022-10-10 17:08:29 +0800

    Confirm part1

--- hw1/part1/get_mean_std.py
+++ hw1/part1/get_mean_std.py
@@ -12,7 +12,25 @@ def get_mean_std(dataset):
     return mean, std
 
 
+def double_check():
+    import pandas as pd
+    df = pd.read_csv('val_gt.csv')
+
+    count = 0
+    data = df.to_numpy()
+    for row in data:
+        name = row[0]
+        label = row[1]
+        if int(name.split('_')[0]) == label:
+            count += 1
+    print(count, data.shape)
+
+    df.info()
+
+
 if __name__ == '__main__':
+    # double_check()
+
     train_set = part1_dataset(prefix='./data/p1_data/train_50')
     mean, std = get_mean_std(train_set)
     print(mean, std)
