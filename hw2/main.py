f6c245de1a77c47f7dfa6390ddaf169e6708839d
IanLynnEE <kmes50311@gmail.com>
2022-10-30 02:40:24 +0800

    Resume from checkpoint

--- hw2/main.py
+++ hw2/main.py
@@ -7,20 +7,26 @@ from torchvision.transforms import transforms
 
 from models import DCGANGenerator, DCGANDiscriminator
 from datasets import FaceDataset
-from tools import train_GAN
+from tools import train_GAN, load_checkpoint
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data_root', type=str, default='./hw2_data/face/')
-    parser.add_argument('--num_classes', type=int, default=10)
+    parser.add_argument('--prefix', type=str, default='./hw2_data/face/')
+    parser.add_argument('--use_checkpoint', type=str, default='None')
     parser.add_argument('--batch_size', type=int, default=256)
-    parser.add_argument('--num_epochs', type=int, default=100)
-    parser.add_argument('--learning_rate', type=float, default=0.0004)
+    parser.add_argument('--epochs', type=int, default=100)
+    parser.add_argument('--lr', type=float, default=0.0004)
     args = parser.parse_args()
 
     device = torch.device('cuda' if torch.cuda.is_available else 'cpu')
 
+    if 'face' in args.prefix:
+        train_GAN(device, *setup_DCGAN(args))
+    return
+
+
+def setup_DCGAN(args):
     # https://gist.github.com/weiaicunzai/e623931921efefd4c331622c344d8151
     means = [0.5071, 0.4865, 0.4409]
     stds = [0.2673, 0.2564, 0.2762]
@@ -29,22 +35,31 @@ def main():
         transforms.Normalize(means, stds),
         transforms.RandomHorizontalFlip(),
     ])
-    train_dir = os.path.join(args.data_root, 'train')
+    train_dir = os.path.join(args.prefix, 'train')
     train_set = FaceDataset(prefix=train_dir, trans=trans)
     loader = DataLoader(train_set, batch_size=args.batch_size, shuffle=True)
 
-    gen = DCGANGenerator()
-    dis = DCGANDiscriminator()
-    optimizers = (
-        torch.optim.AdamW(gen.parameters(), lr=args.learning_rate),
-        torch.optim.AdamW(dis.parameters(), lr=args.learning_rate)
+    models = (
+        DCGANGenerator(),
+        DCGANDiscriminator()
     )
     criterions = (
         torch.nn.BCELoss(),
         torch.nn.BCELoss()
     )
-    train_GAN(device, loader, (gen, dis), criterions, optimizers, args.num_epochs)
-    return
+    optimizers = (
+        torch.optim.AdamW(models[0].parameters(), lr=args.lr),
+        torch.optim.AdamW(models[1].parameters(), lr=args.lr)
+    )
+    epochs = range(args.epochs)
+    if args.use_checkpoint == 'DCGAN':
+        path = './saved_models/DCGANGenerator.pt'
+        epoch = load_checkpoint(path, models[0], optimizers[0])
+        path = './saved_models/DCGANDiscriminator.pt'
+        if epoch != load_checkpoint(path, models[1], optimizers[1]):
+            print('Warning: Using checkpoints from different epoch')
+        epochs = range(epoch + 1, epoch + 1 + args.epochs)
+    return loader, models, criterions, optimizers, epochs
 
 
 if __name__ == '__main__':
--- hw2/tools.py
+++ hw2/tools.py
@@ -11,7 +11,7 @@ def fix_seed(seed):
     pass
 
 
-def save_checkpoint(epoch, model, optimizer, loss):
+def save_checkpoint(epoch, model, optimizer):
     path = f'saved_models/{model.__class__.__name__}_{epoch}.pt'
     print(f'Saving model to {path}...')
     torch.save(
@@ -19,14 +19,37 @@ def save_checkpoint(epoch, model, optimizer, loss):
             'epoch': epoch,
             'model_state_dict': model.state_dict(),
             'optimizer_state_dict': optimizer.state_dict(),
-            'loss': loss,
         },
         path
     )
 
 
+def load_checkpoint(path, model, optimizer):
+    checkpoint = torch.load(path)
+    model.load_state_dict(checkpoint['model_state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    return checkpoint['epoch']
+
+
+# https://github.com/pytorch/pytorch/issues/8741
+def optimizer_to(optim, device):
+    for param in optim.state.values():
+        if isinstance(param, torch.Tensor):
+            param.data = param.data.to(device)
+            if param._grad is not None:
+                param._grad.data = param._grad.data.to(device)
+        elif isinstance(param, dict):
+            for subparam in param.values():
+                if isinstance(subparam, torch.Tensor):
+                    subparam.data = subparam.data.to(device)
+                    if subparam._grad is not None:
+                        subparam._grad.data = subparam._grad.data.to(device)
+    return
+
+
 def train_GAN(device, loader, models, criterions, optimizers, epochs):
     writer = SummaryWriter('saved_models/')
+    best_score = 0.0
 
     generator = models[0]
     discriminator = models[1]
@@ -35,8 +58,10 @@ def train_GAN(device, loader, models, criterions, optimizers, epochs):
 
     optimizer_g = optimizers[0]
     optimizer_d = optimizers[1]
+    optimizer_to(optimizer_g, device)
+    optimizer_to(optimizer_d, device)
 
-    for epoch in range(epochs):
+    for epoch in epochs:
         sum_loss_d_real = 0.0
         sum_loss_d_fake = 0.0
         sum_loss_g = 0.0
@@ -47,20 +72,19 @@ def train_GAN(device, loader, models, criterions, optimizers, epochs):
             data = data.to(device)
             batch_size = data.size(0)
 
-            # TODO: Soft label or not?
-            label_real = 1.0 - 0.3 * torch.rand((batch_size, 1, 1, 1), dtype=torch.float, device=device)
-            label_fake = 0.0 + 0.3 * torch.rand((batch_size, 1, 1, 1), dtype=torch.float, device=device)
+            positive = 1.0 - 0.3 * torch.rand((batch_size, 1, 1, 1), dtype=torch.float, device=device)
+            negative = 0.0 + 0.3 * torch.rand((batch_size, 1, 1, 1), dtype=torch.float, device=device)
             noise = torch.randn((batch_size, 100, 1, 1), device=device)
 
             # Train discriminator
             optimizer_d.zero_grad()
             output = discriminator(data)
-            loss_d_real = criterions[1](output, label_real)
+            loss_d_real = criterions[1](output, positive)
             sum_loss_d_real += loss_d_real.item()
 
             image_fake = generator(noise)
             output = discriminator(image_fake)
-            loss_d_fake = criterions[1](output, label_fake)
+            loss_d_fake = criterions[1](output, negative)
             sum_loss_d_fake += loss_d_fake.item()
 
             loss_d = loss_d_real + loss_d_fake
@@ -70,14 +94,13 @@ def train_GAN(device, loader, models, criterions, optimizers, epochs):
             # Train generator
             optimizer_g.zero_grad()
             output = discriminator(image_fake)
-            loss_g = criterions[0](output, label_real)
+            loss_g = criterions[0](output, positive)
             sum_loss_g += loss_g.item()
             loss_g.backward()
             optimizer_g.step()
 
         generator.eval()
         with torch.no_grad():
-            # TODO: Reduce batch size to speed up.
             noise = torch.randn((1000, 100, 1, 1), device=device)
             output = generator(noise)
             for i, image in enumerate(output):
@@ -90,7 +113,7 @@ def train_GAN(device, loader, models, criterions, optimizers, epochs):
             2048
         )
         hog = face_recog('outputs/hw2_1/')
-        print(f'fid = {fid:3.2f}, hog = {hog:3.2f}')
+        print(f'epoch = {epoch:3d}, fid = {fid:3.2f}, hog = {hog:3.2f}')
 
         writer.add_scalar('loss_d/loss_d_real', sum_loss_d_real, epoch)
         writer.add_scalar('loss_d/loss_d_fake', sum_loss_d_fake, epoch)
@@ -98,7 +121,8 @@ def train_GAN(device, loader, models, criterions, optimizers, epochs):
         writer.add_scalar('FID', fid, epoch)
         writer.add_scalar('HOG', hog, epoch)
 
-        if epoch % 5 == 0:
-            save_checkpoint(epoch, generator, optimizer_g, loss_g)
-            save_checkpoint(epoch, discriminator, optimizer_d, loss_d)
+        if (epoch % 5 == 0) or (best_score < fid):
+            save_checkpoint(epoch, generator, optimizer_g)
+            save_checkpoint(epoch, discriminator, optimizer_d)
+            best_score = fid if best_score < fid else None
     writer.close()
